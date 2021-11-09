a1b1d53be8bd413dcf1a80fd6c544a2a47ddd7e4
Zahi Farah <zahi.farah@protonmail.com>
2021-11-09 19:28:05 +0100

    Complete task #1 and #2

--- elyses-enchantments/enchantments.js
+++ elyses-enchantments/enchantments.js
@@ -9,7 +9,7 @@
  * @returns {number} the card
  */
 export function getItem(cards, position) {
-  throw new Error('Implement the getItem function');
+  return cards[position]
 }
 
 /**
@@ -22,7 +22,8 @@ export function getItem(cards, position) {
  * @returns {number[]} the cards with the change applied
  */
 export function setItem(cards, position, replacementCard) {
-  throw new Error('Implement the setItem function');
+  cards.splice(position, 1, replacementCard)
+  return cards
 }
 
 /**
@@ -34,7 +35,7 @@ export function setItem(cards, position, replacementCard) {
  * @returns {number[]} the cards with the newCard applied
  */
 export function insertItemAtTop(cards, newCard) {
-  throw new Error('Implement the insertItemAtTop function');
+  throw new Error("Implement the insertItemAtTop function")
 }
 
 /**
@@ -46,7 +47,7 @@ export function insertItemAtTop(cards, newCard) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItem(cards, position) {
-  throw new Error('Implement the removeItem function');
+  throw new Error("Implement the removeItem function")
 }
 
 /**
@@ -57,7 +58,7 @@ export function removeItem(cards, position) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItemFromTop(cards) {
-  throw new Error('Implement the removeItemFromTop function');
+  throw new Error("Implement the removeItemFromTop function")
 }
 
 /**
@@ -69,7 +70,7 @@ export function removeItemFromTop(cards) {
  * @returns {number[]} the cards including the new card
  */
 export function insertItemAtBottom(cards, newCard) {
-  throw new Error('Implement the insertItemAtBottom function');
+  throw new Error("Implement the insertItemAtBottom function")
 }
 
 /**
@@ -80,7 +81,7 @@ export function insertItemAtBottom(cards, newCard) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItemAtBottom(cards) {
-  throw new Error('Implement the removeItemAtBottom function');
+  throw new Error("Implement the removeItemAtBottom function")
 }
 
 /**
@@ -92,5 +93,5 @@ export function removeItemAtBottom(cards) {
  * @returns {boolean} true if there are exactly stackSize number of cards, false otherwise
  */
 export function checkSizeOfStack(cards, stackSize) {
-  throw new Error('Implement the checkSizeOfStack function');
+  throw new Error("Implement the checkSizeOfStack function")
 }
