76c342519f1fc220797c585d1b633b1ff81d83b1
Zahi Farah <zahi.farah@protonmail.com>
2021-11-10 12:53:53 +0100

    Pass all tests

--- vehicle-purchase/vehicle-purchase.js
+++ vehicle-purchase/vehicle-purchase.js
@@ -1,9 +1,3 @@
-// @ts-check
-//
-// The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion when
-// implementing this exercise.
-
 /**
  * Determines whether or not you need a licence to operate a certain kind of vehicle.
  *
@@ -11,7 +5,7 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
+  return kind === "car" || kind === "truck"
 }
 
 /**
@@ -23,7 +17,10 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
+  if (option1 < option2) {
+    return `${option1} is clearly the better choice.`
+  }
+  return `${option2} is clearly the better choice.`
 }
 
 /**
@@ -35,5 +32,11 @@ export function chooseVehicle(option1, option2) {
  * @returns expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  if (age >= 3 && age <= 10) {
+    return (originalPrice = originalPrice * 0.7)
+  } else if (age < 3) {
+    return (originalPrice = originalPrice * 0.8)
+  } else {
+    return (originalPrice = originalPrice * 0.5)
+  }
 }
