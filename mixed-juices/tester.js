8f75df717071b41f0c8d6e4b38f71ac39d9d75a1
Zahi Farah <zahi.farah@protonmail.com>
2021-11-11 12:41:20 +0100

    Use break

--- mixed-juices/tester.js
+++ mixed-juices/tester.js
@@ -44,9 +44,8 @@ function limesToCut(wedgesNeeded, limes) {
       }
       neededLimes++
     } else {
-      return neededLimes
+      break
     }
-    console.log(`${lime}`)
   }
 
   return neededLimes
