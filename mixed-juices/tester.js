5415debf1c7c62212548350fa868bc1a18f20de6
Zahi Farah <zahi.farah@protonmail.com>
2021-11-12 12:44:31 +0100

    Add comments

--- mixed-juices/tester.js
+++ mixed-juices/tester.js
@@ -10,12 +10,12 @@ function remainingOrders(timeLeft, orders) {
   let i = 0
   let timeElapsed = 0
 
-  while (timeLeft > 0) {
-    if (i < orders.length) {
-      console.log(timeLeft)
-      console.log(timeElapsed)
-      console.log(i)
-      console.log(orders[i])
+  while (i < orders.length) {
+    if (timeLeft > 0) {
+      console.log(timeLeft) // 13, 10, 8.5, 6, 1
+      console.log(timeElapsed) // 0, 3, 1.5, 2.5, 5 <-- should start with a value of 3, not 0
+      console.log(i) // 0, 1, 2, 3, 4 <-- makes sense
+      console.log(orders[i]) // iterates over all elements in orders array
       switch (orders[i]) {
         case "Pure Strawberry Joy":
           timeElapsed = 0.5
@@ -45,7 +45,7 @@ function remainingOrders(timeLeft, orders) {
     }
     console.log(timeLeft)
     console.log(timeElapsed)
-    console.log(i)
+    console.log(i) // 1, 2, 3, 4, 5 <-- no idea why this is returning 5? Which is greater than orders.length?
     console.log(orders[i])
   }
 
